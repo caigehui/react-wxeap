7e4b4b7b851fd580ddfceca2a090c3f8c07eb7d3
caigehui <1136687@qq.com>
2017-03-24 20:19:36 +0800

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,9 @@
-# 2017-3-24 (version 0.3.25)
+# 2017-3-24 (version 0.3.27)
 * 新增两个数组的工具方法
 * 新增Array的removeObjectsByCondition
 * 对字体进行修改
 * 列表边框调整
+* 新增Seperator分割线
 
 # 2017-3-22 (version 0.3.15)
 * 引入babel-polyfill解决Android浏览器找不到Symbol的问题
--- src/components/Seperator/index.js
+++ src/components/Seperator/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default class Seperator extends React.Component {
 
-    render() {
+    render() {        
         return (
-            <div style={{ height: 1, width: '100%', borderBottom: '1px solid rgb(230,230,235)' }} />
+            <div style={{ height: 1, width: '100%', borderBottom: '1px solid rgb(230,230,235)' ,...this.props.style}} />
         )
     }
 }

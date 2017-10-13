971f3a0eeb069e487ce100858eb2fec1c735db3f
Mathieu-Lu <lsqup0702@hotmail.com>
2017-10-13 14:47:55 +0800

    update react-wxeap gh-pages
    用于展示react-wxeap框架的在线示例介绍

--- docs/utils/htmlFilter.md
+++
@@ -1,8 +0,0 @@
-# htmlFilter
-过滤富文本中的html标签并转义特殊字符
-```javascript
-import { htmlFilter } from 'react-wxeap';
-
-let ret = htmlFilter(str);
-
-```
--- src/utils/backToHome.js
+++
@@ -1,8 +0,0 @@
-import { DEV_MODE } from '../constants';
-export default function backToHome() {
-    if(DEV_MODE) {
-        const config = require('../../../../config/default.json');
-        return location.href = `${config.origin}/Base/Main/WxTouchMain.aspx`;
-    }
-    return location.href = `${EAP}Base/Main/WxTouchMain.aspx`;
-}

8fe58509f162fda444e3729430bd9dc5fe58da36
MagicYoung <734846967@qq.com>
2017-10-19 11:23:55 +0800

    update search compoment (#33)
    * update Search Compoment
    
    update Search Compoment ,add  searchCondtion
    
    * update Search(sup)

--- src/components/Search/model.js
+++ src/components/Search/model.js
@@ -1,53 +1,67 @@
 export default {
-
-    namespace: 'searchComponent',
-
-    state: {
-        onSearch: () => ({}),
-        renderRow: () => ({}),
-        onCancel: () => ({}),
-        label: '查找内容',
-        notFoundLabel: '未找到相关内容',
-        placeholder: '搜索',
-        isEmpty: false, // 搜索结果是否为空
-        isInit: true, // 是否初始化
-        content: '',
-        autoFocus: true, // 自动聚焦
-        isBack: false
-    },
-
-    subscriptions: {
-        setup({ history, dispatch }) {
-            return history.listen(({ pathname, state }) => {
-                if (pathname === '/SearchComponent') {
-                    if (!state.onSearch) return dispatch({
+    
+        namespace: 'searchComponent',
+    
+        state: {
+            onSearch: () => ({}),
+            renderRow: () => ({}),
+            onCancel: () => ({}),
+            label: '查找内容',
+            notFoundLabel: '未找到相关内容',
+            placeholder: '搜索',
+            isEmpty: false, // 搜索结果是否为空
+            isInit: true, // 是否初始化
+            content: '',
+            autoFocus: true, // 自动聚焦
+            isBack: false,
+            searchCondition: [],// 所有查询条件
+            condition: []// 当前选中的查询条件[{label:'',value:''}]
+        },
+        effects: {
+            *onPushToRoute({ payload: { current, before } }, { put,select }) {
+                const state = yield select(state => state.searchComponent);
+                if (!state.onSearch) {
+                    yield put({
                         type: 'save',
                         payload: {
                             autoFocus: false,
                             isBack: true
                         }
                     });
-                    dispatch({
+                }else{
+                    yield put({
                         type: 'save',
                         payload: {
-                            ...state,
                             content: '',
                             autoFocus: true,
                             isInit: true,
                             isEmpty: false,
-                            isBack: true
+                            isBack: true,
+                            condition: [],
                         }
                     });
                 }
-            });
-        }
-    },
-    reducers: {
-        save(state, action) {
-            return { ...state, ...action.payload };
-        }
-    },
-    effects: {
-
-    }
-};
+                
+            },
+            // *onPopToRoute({ payload: { before, current } }, { put,select }) {
+            //     const state = yield select(state => state.searchComponent);
+            //     console.log(state);
+            // },
+            // *routeDidPop({ payload: { current, next } }, { put }) {
+            //     yield put({
+            //         type: 'save',
+            //         payload: {
+            //           condition:[],
+            //         }
+            //     });
+            // }
+        },
+        reducers: {
+            save(state, action) {
+                return { ...state, ...action.payload };
+            }
+        },
+        // 是否自动恢复state
+        // persist: true
+    };
+    
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -11,74 +11,77 @@ import bind from '../../app/bind';
 
 @bind()
 export default class SearchBar extends Component {
-
-    static propTypes = {
-        width: PropTypes.any, // 搜索条宽度
-        disabled: PropTypes.bool, // 是否禁用
-        onClick: PropTypes.func, // 点击SeachBar
-        onSearch: PropTypes.func, // 触发搜索
-        onCancel: PropTypes.func,// 取消查找触发的回调
-        renderRow: PropTypes.func,// 渲染每一行
-        placeholder: PropTypes.string,// 输入框默认的搜索数据
-        label: PropTypes.string,// 默认下方icon提示文字
-        notFoundLabel: PropTypes.string,// 未找到数据的提示文字
-        style: PropTypes.object, // 样式覆盖
-        dispatch: PropTypes.any
+    
+        static propTypes = {
+            width: PropTypes.any, // 搜索条宽度
+            disabled: PropTypes.bool, // 是否禁用
+            onClick: PropTypes.func, // 点击SeachBar
+            onSearch: PropTypes.func, // 触发搜索
+            onCancel: PropTypes.func,// 取消查找触发的回调
+            renderRow: PropTypes.func,// 渲染每一行
+            placeholder: PropTypes.string,// 输入框默认的搜索数据
+            label: PropTypes.string,// 默认下方icon提示文字
+            notFoundLabel: PropTypes.string,// 未找到数据的提示文字
+            style: PropTypes.object, // 样式覆盖
+            dispatch: PropTypes.any,
+            searchCondition: PropTypes.any
+        }
+    
+        static defaultProps = {
+            width: '100%'
+        }
+        onClick = () => {
+            // 禁用
+            if (this.props.disabled) return;
+            this.props.onClick && this.props.onClick();
+            this.props.dispatch({
+                type: 'searchComponent/save',
+                payload: {
+                    onSearch: this.props.onSearch,
+                    renderRow: this.props.renderRow,
+                    placeholder: this.props.placeholder,
+                    label: this.props.label,
+                    notFoundLabel: this.props.notFoundLabel,
+                    onCancel: this.props.onCancel,
+                    searchCondition: this.props.searchCondition
+                }
+            });
+            this.props.dispatch(routerRedux.push({
+                pathname: '/searchComponent',
+            }));
+        }
+    
+        render() {
+            const { width, style } = this.props;
+            return (
+                <View
+                    style={this.props.disabled ? { ...styles.container, ...styles.disabled, width, ...style } : { ...styles.container, width, ...style }}
+                    onClick={this.onClick}>
+                    <img src={require('../../assets/search.png')} style={styles.icon} />
+                    搜索
+                </View>
+            );
+        }
     }
-
-    static defaultProps = {
-        width: '100%'
-    }
-
-    onClick = () => {
-        // 禁用
-        if (this.props.disabled) return;
-        this.props.onClick && this.props.onClick();
-
-        this.props.dispatch(routerRedux.push({
-            pathname: '/SearchComponent',
-            state: {
-                onSearch: this.props.onSearch,
-                renderRow: this.props.renderRow,
-                placeholder: this.props.placeholder,
-                label: this.props.label,
-                notFoundLabel: this.props.notFoundLabel,
-                onCancel: this.props.onCancel
-            }
-        }));
-    }
-
-    render() {
-        const { width, style } = this.props;
-        return (
-            <View
-                style={this.props.disabled ? { ...styles.container, ...styles.disabled, width, ...style } : { ...styles.container, width, ...style }}
-                onClick={this.onClick}>
-                <img src={require('../../assets/search.png')} style={styles.icon} />
-                搜索
-            </View>
-        );
-    }
-}
-
-const styles = {
-    container: {
-        height: BAR_HEIGHT,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: COLORS.BACKGROUND_COLOR,
-        color: COLORS.SUBTITLE_COLOR,
-        borderRadius: BORDER_DADIUS,
-        fontSize: 26
-    },
-    disabled: {
-        color: COLORS.BACKGROUND_COLOR,
-        opacity: DISABLED_OPACITY,
-        backgroundColor: COLORS.SUBTITLE_COLOR
-    },
-    icon: {
-        height: 36,
-        width: 36,
-        marginRight: 10
-    }
-};
+    
+    const styles = {
+        container: {
+            height: BAR_HEIGHT,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.BACKGROUND_COLOR,
+            color: COLORS.SUBTITLE_COLOR,
+            borderRadius: BORDER_DADIUS,
+            fontSize: 26
+        },
+        disabled: {
+            color: COLORS.BACKGROUND_COLOR,
+            opacity: DISABLED_OPACITY,
+            backgroundColor: COLORS.SUBTITLE_COLOR
+        },
+        icon: {
+            height: 36,
+            width: 36,
+            marginRight: 10
+        }
+    };
